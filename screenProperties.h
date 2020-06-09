cdafdd582143cf6bea1c1476086c490321aad73c
garyjwalker <garyjwalker42@gmail.com>
2020-06-08 23:06:11 -0500

    Moved some properties outside to external file.

---
+++ screenProperties.h
@@ -0,0 +1,5 @@
+constexpr int screenWidth {64};
+constexpr int screenHeight {32};
+constexpr int pixelSize {4};
+const sf::Color colorOn {sf::Color::White};
+const sf::Color colorOff {sf::Color::Black};
